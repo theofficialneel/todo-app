f9e7f81f0000e002e7a8a82020de9639a47b0acb
neel <theofficialneel@gmail.com>
2018-10-19 23:15:32 +0530

    dynamic list

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,6 +1,7 @@
+let data = [{item: "Hello world"}];
 module.exports = function (app) {
     app.get("/todo",function (req,res) {
-        res.render("todo");
+        res.render("todo",{todo_data: data});
     });
     app.post("/todo",function (req,res) {
 
